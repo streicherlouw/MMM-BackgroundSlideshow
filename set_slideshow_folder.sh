7f22fc595b517d7c93b2c2d125e5899feaecfba7
streicherlouw <streicher@streicher.dk>
2020-07-14 14:19:30 +1000

    Initial commit

---
+++ set_slideshow_folder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+path_to_images_prefix="\['modules\/MMM-BackgroundSlideshow\/"
+path_to_images_postfix="'\]\,"
+replacement=$1
+replacement=${replacement//\\/\\\\}
+replacement=${replacement//\//\\/}
+replacement=${replacement//&/\\&}
+path_to_insert=${replacement//$'\n'/\\$'\n'}
+fullpath=$path_to_images_prefix$path_to_insert$path_to_images_postfix
+sed -i "s/\(^[[:space:]]*imagePaths:\).*/\1 $fullpath/" ~/MagicMirror/config/config.js
+pm2 restart ~/MagicMirror/installers/mm.sh
